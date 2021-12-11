f23ee893fa8729f57f697aa5acd4b9b13ae4e4dd
MiGoYAm <miroslawjakiela@gmail.com>
2021-12-11 17:48:40 +0100

    Revert "Revert "Added display of brand name under f3""
    This reverts commit 974e243402e0838db98716c7efba9da85b07bcf9.

--- src/main/java/ru/nanit/limbo/connection/ClientConnection.java
+++ src/main/java/ru/nanit/limbo/connection/ClientConnection.java
@@ -40,6 +40,7 @@ public class ClientConnection extends ChannelInboundHandlerAdapter {
 
     private static PreEncodedPacket PACKET_LOGIN_SUCCESS;
     private static PreEncodedPacket PACKET_JOIN_GAME;
+    private static PreEncodedPacket PACKET_PLUGIN_MESSAGE;
     private static PreEncodedPacket PACKET_PLAYER_ABILITIES;
     private static PreEncodedPacket PACKET_PLAYER_INFO;
     private static PreEncodedPacket PACKET_PLAYER_POS;
@@ -211,6 +212,7 @@ public class ClientConnection extends ChannelInboundHandlerAdapter {
         server.getConnections().addConnection(this);
 
         writePacket(PACKET_JOIN_GAME);
+        writePacket(PACKET_PLUGIN_MESSAGE);
         writePacket(PACKET_PLAYER_ABILITIES);
         writePacket(PACKET_PLAYER_POS);
         writePacket(PACKET_PLAYER_INFO);
