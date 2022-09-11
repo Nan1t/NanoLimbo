176352261887929ce49e04bcaa9e92457b510ccf
U61vashka <botyrbojey@gmail.com>
2022-09-11 23:54:56 +0600

    Configuration interface, LimboConfig -> YamlLimboConfig

--- src/main/java/ru/nanit/limbo/configuration/LimboConfig.java
+++ src/main/java/ru/nanit/limbo/configuration/LimboConfig.java
@@ -1,258 +1,61 @@
-/*
- * Copyright (C) 2020 Nan1t
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <https://www.gnu.org/licenses/>.
- */
-
 package ru.nanit.limbo.configuration;
 
-import org.spongepowered.configurate.ConfigurationNode;
-import org.spongepowered.configurate.ConfigurationOptions;
-import org.spongepowered.configurate.serialize.TypeSerializerCollection;
-import org.spongepowered.configurate.yaml.YamlConfigurationLoader;
-import ru.nanit.limbo.server.data.*;
-import ru.nanit.limbo.util.Colors;
+import java.net.SocketAddress;
+
+import ru.nanit.limbo.server.data.BossBar;
+import ru.nanit.limbo.server.data.InfoForwarding;
+import ru.nanit.limbo.server.data.PingData;
+import ru.nanit.limbo.server.data.Title;
 import ru.nanit.limbo.world.Location;
 
-import java.io.BufferedReader;
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.io.InputStream;
-import java.net.SocketAddress;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
+public interface LimboConfig {
+    SocketAddress getAddress();
 
-public final class LimboConfig {
+    int getMaxPlayers();
 
-    private final Path root;
+    PingData getPingData();
 
-    private SocketAddress address;
-    private int maxPlayers;
-    private PingData pingData;
+    String getDimensionType();
 
-    private String dimensionType;
-    private Location spawnPosition;
-    private int gameMode;
+    Location getSpawnPosition();
 
-    private boolean useBrandName;
-    private boolean useJoinMessage;
-    private boolean useBossBar;
-    private boolean useTitle;
-    private boolean usePlayerList;
-    private boolean useHeaderAndFooter;
+    int getGameMode();
 
-    private String brandName;
-    private String joinMessage;
-    private BossBar bossBar;
-    private Title title;
+    InfoForwarding getInfoForwarding();
 
-    private String playerListUsername;
-    private String playerListHeader;
-    private String playerListFooter;
+    long getReadTimeout();
 
-    private InfoForwarding infoForwarding;
-    private long readTimeout;
-    private int debugLevel;
+    int getDebugLevel();
 
-    private boolean useEpoll;
-    private int bossGroupSize;
-    private int workerGroupSize;
+    boolean isUseBrandName();
 
-    public LimboConfig(Path root) {
-        this.root = root;
-    }
+    boolean isUseJoinMessage();
 
-    public void load() throws Exception {
-        ConfigurationOptions options = ConfigurationOptions.defaults().serializers(getSerializers());
-        YamlConfigurationLoader loader = YamlConfigurationLoader.builder()
-                .source(this::getReader)
-                .defaultOptions(options)
-                .build();
+    boolean isUseBossBar();
 
-        ConfigurationNode conf = loader.load();
+    boolean isUseTitle();
 
-        address = conf.node("bind").get(SocketAddress.class);
-        maxPlayers = conf.node("maxPlayers").getInt();
-        pingData = conf.node("ping").get(PingData.class);
-        dimensionType = conf.node("dimension").getString();
-        if (dimensionType.equalsIgnoreCase("nether")) {
-            dimensionType = "the_nether";
-        }
-        if (dimensionType.equalsIgnoreCase("end")) {
-            dimensionType = "the_end";
-        }
-        spawnPosition = conf.node("spawnPosition").get(Location.class);
-        gameMode = conf.node("gameMode").getInt();
-        useBrandName = conf.node("brandName", "enable").getBoolean();
-        useJoinMessage = conf.node("joinMessage", "enable").getBoolean();
-        useBossBar = conf.node("bossBar", "enable").getBoolean();
-        useTitle = conf.node("title", "enable").getBoolean();
-        usePlayerList = conf.node("playerList", "enable").getBoolean();
-        playerListUsername = conf.node("playerList", "username").getString();
-        useHeaderAndFooter = conf.node("headerAndFooter", "enable").getBoolean();
+    boolean isUsePlayerList();
 
-        if (useBrandName)
-            brandName = conf.node("brandName", "content").getString();
+    boolean isUseHeaderAndFooter();
 
-        if (useJoinMessage)
-            joinMessage = Colors.of(conf.node("joinMessage", "text").getString(""));
+    String getBrandName();
 
-        if (useBossBar)
-            bossBar = conf.node("bossBar").get(BossBar.class);
+    String getJoinMessage();
 
-        if (useTitle)
-            title = conf.node("title").get(Title.class);
+    BossBar getBossBar();
 
-        if (useHeaderAndFooter) {
-            playerListHeader = Colors.of(conf.node("headerAndFooter", "header").getString());
-            playerListFooter = Colors.of(conf.node("headerAndFooter", "footer").getString());
-        }
+    Title getTitle();
 
-        infoForwarding = conf.node("infoForwarding").get(InfoForwarding.class);
-        readTimeout = conf.node("readTimeout").getLong();
-        debugLevel = conf.node("debugLevel").getInt();
+    String getPlayerListUsername();
 
-        useEpoll = conf.node("netty", "useEpoll").getBoolean(true);
-        bossGroupSize = conf.node("netty", "threads", "bossGroup").getInt(1);
-        workerGroupSize = conf.node("netty", "threads", "workerGroup").getInt(4);
-    }
+    String getPlayerListHeader();
 
-    private BufferedReader getReader() throws IOException {
-        String name = "settings.yml";
-        Path filePath = Paths.get(root.toString(), name);
+    String getPlayerListFooter();
 
-        if (!Files.exists(filePath)) {
-            InputStream stream = getClass().getResourceAsStream( "/" + name);
+    boolean isUseEpoll();
 
-            if (stream == null)
-                throw new FileNotFoundException("Cannot find settings resource file");
+    int getBossGroupSize();
 
-            Files.copy(stream, filePath);
-        }
-
-        return Files.newBufferedReader(filePath);
-    }
-
-    private TypeSerializerCollection getSerializers() {
-        return TypeSerializerCollection.builder()
-                .register(SocketAddress.class, new SocketAddressSerializer())
-                .register(InfoForwarding.class, new InfoForwarding.Serializer())
-                .register(PingData.class, new PingData.Serializer())
-                .register(BossBar.class, new BossBar.Serializer())
-                .register(Title.class, new Title.Serializer())
-                .register(Location.class, new Location.Serializer())
-                .build();
-    }
-
-    public SocketAddress getAddress() {
-        return address;
-    }
-
-    public int getMaxPlayers() {
-        return maxPlayers;
-    }
-
-    public PingData getPingData() {
-        return pingData;
-    }
-
-    public String getDimensionType() {
-        return dimensionType;
-    }
-
-    public Location getSpawnPosition() {
-        return spawnPosition;
-    }
-
-    public int getGameMode() {
-        return gameMode;
-    }
-
-    public InfoForwarding getInfoForwarding() {
-        return infoForwarding;
-    }
-
-    public long getReadTimeout() {
-        return readTimeout;
-    }
-
-    public int getDebugLevel() {
-        return debugLevel;
-    }
-
-    public boolean isUseBrandName() {
-        return useBrandName;
-    }
-
-    public boolean isUseJoinMessage() {
-        return useJoinMessage;
-    }
-
-    public boolean isUseBossBar() {
-        return useBossBar;
-    }
-
-    public boolean isUseTitle() {
-        return useTitle;
-    }
-
-    public boolean isUsePlayerList() {
-        return usePlayerList;
-    }
-
-    public boolean isUseHeaderAndFooter() {
-        return useHeaderAndFooter;
-    }
-
-    public String getBrandName() {
-        return brandName;
-    }
-
-    public String getJoinMessage() {
-        return joinMessage;
-    }
-
-    public BossBar getBossBar() {
-        return bossBar;
-    }
-
-    public Title getTitle() {
-        return title;
-    }
-
-    public String getPlayerListUsername() {
-        return playerListUsername;
-    }
-
-    public String getPlayerListHeader() {
-        return playerListHeader;
-    }
-
-    public String getPlayerListFooter() {
-        return playerListFooter;
-    }
-
-    public boolean isUseEpoll() {
-        return useEpoll;
-    }
-
-    public int getBossGroupSize() {
-        return bossGroupSize;
-    }
-
-    public int getWorkerGroupSize() {
-        return workerGroupSize;
-    }
+    int getWorkerGroupSize();
 }
